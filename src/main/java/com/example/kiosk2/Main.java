b8484e9e9cf0432e4f47a96107a92bb8983029a7
seaking110 <seaking110@naver.com>
2025-01-14 15:19:56 +0900

    Lv 2. 객체 지향 설계를 적용해 햄버거 메뉴를 클래스로 관리하기

---
+++ src/main/java/com/example/kiosk2/Main.java
@@ -0,0 +1,36 @@
+package com.example.kiosk2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List <MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("Thigh Burger",4.9, "바삭한 치킨 패티와 양상추가 조화를 이루는 맘스터치 시그니처 버거."));
+        menuItems.add(new MenuItem("Fire Thigh Burger",5.1, "불맛이 살아있는 버거, 싸이버거의 매운맛 버전."));
+        menuItems.add(new MenuItem("White Garlic Burger",5.5, "화이트 갈릭소스에 더블햄과 통가슴살 패티까지 담은 버거."));
+        menuItems.add(new MenuItem("Deep Cheese Burger",5.4, "부드러운 치즈와 통가슴살 패티 버거."));
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("[ MOMS TOUCH MENU ]");
+            int count = 1;
+            for (MenuItem menu :menuItems) {
+                System.out.printf("%d. %-20s | W %.1f | %s\n",count,menu.name, menu.price,menu.text);
+                count++;
+            }
+            System.out.println("0. 종료     | 종료 ");
+            int n = sc.nextInt();
+            if (n == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else if (n > menuItems.size()) {
+                System.out.println("잘못된 번호를 입력했습니다.");
+            } else {
+                System.out.printf("선택한 메뉴 : %s | W %.1f | %s\n",menuItems.get(n-1).name, menuItems.get(n-1).price,menuItems.get(n-1).text);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/example/kiosk2/MenuItem.java
@@ -0,0 +1,12 @@
+package com.example.kiosk2;
+
+public class MenuItem {
+    String name;
+    double price;
+    String text;
+    public MenuItem(String name, double price, String text) {
+        this.name = name;
+        this.price = price;
+        this.text = text;
+    }
+}
